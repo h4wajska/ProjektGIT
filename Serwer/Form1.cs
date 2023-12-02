e00c6cc0772938ccf07f6ac16821f106e9a9f57a
Bartosz Kluj <bartoszkluj@gmail.com>
2023-12-02 11:31:59 +0100

    1st commit

---
+++ Serwer/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Serwer
+{
+    public partial class Serwer : Form
+    {
+        public Serwer()
+        {
+            InitializeComponent();
+        }
+
+        private void Serwer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
